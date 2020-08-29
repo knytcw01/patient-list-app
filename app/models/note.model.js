15f523ce37c985a8363adaac5b62468c0660cfe8
knytcw01 <35956832+knytcw01@users.noreply.github.com>
2020-08-29 14:12:31 +0800

    Update note.model.js

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const Patient = new Schema(
+const PatientSchema = new Schema(
     {
         name: { type: String, required: true },
-		hkid: { type: String, required: true },
-		dob: { type: String, required: true },
+	hkid: { type: String, required: true },
+	dob: { type: String, required: true },
         country: { type: [String], required: false },
     },
     { timestamps: true },
-)
+);
 
-module.exports = mongoose.model('Patient', Patient)
+module.exports = mongoose.model('Patient', PatientSchema);
