b33c8d40d3741f6682c396f5dab3ff1be196f8e0
knytcw01 <35956832+knytcw01@users.noreply.github.com>
2020-08-29 13:41:17 +0800

    Update note.routes.js

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,18 +1,9 @@
 module.exports = (app) => {
-    const notes = require('../controllers/note.controller.js');
+    const PatientCtrl = require('../controllers/note.controller.js');
 
-    // Create a new Note
-    app.post('/notes', notes.create);
-
-    // Retrieve all Notes
-    app.get('/notes', notes.findAll);
-
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
-
-    // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
-
-    // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
+    app.post('/patient', PatientCtrl.createPatient)
+	app.put('/patient/:id', PatientCtrl.updatePatient)
+	app.delete('/patient/:id', PatientCtrl.deletePatient)
+	app.get('/patient/:id', PatientCtrl.getPatientById)
+	app.get('/patients', PatientCtrl.getPatients)
 }
