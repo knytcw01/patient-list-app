64bad296bbcdbd89898b119d1c18aa5dbacbdd0d
knytcw01 <35956832+knytcw01@users.noreply.github.com>
2020-08-29 13:38:59 +0800

    Update note.controller.js

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,116 +1,125 @@
 const Note = require('../models/note.model.js');
 
-// Create and Save a new Note
-exports.create = (req, res) => {
-    // Validate request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
-        });
+createPatient = (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a patient',
+        })
     }
 
-    // Create a Note
-    const note = new Note({
-        title: req.body.title || "Untitled Note", 
-        content: req.body.content
-    });
+    const patient = new Patient(body)
 
-    // Save Note in the database
-    note.save()
-    .then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
-        });
-    });
-};
-
-// Retrieve and return all notes from the database.
-exports.findAll = (req, res) => {
-    Note.find()
-    .then(notes => {
-        res.send(notes);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving notes."
-        });
-    });
-};
-
-// Find a single note with a noteId
-exports.findOne = (req, res) => {
-    Note.findById(req.params.noteId)
-    .then(note => {
-        if(!note) {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });            
-        }
-        res.send(note);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });                
-        }
-        return res.status(500).send({
-            message: "Error retrieving note with id " + req.params.noteId
-        });
-    });
-};
-
-// Update a note identified by the noteId in the request
-exports.update = (req, res) => {
-    // Validate Request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
-        });
+    if (!patient) {
+        return res.status(400).json({ success: false, error: err })
     }
 
-    // Find note and update it with the request body
-    Note.findByIdAndUpdate(req.params.noteId, {
-        title: req.body.title || "Untitled Note",
-        content: req.body.content
-    }, {new: true})
-    .then(note => {
-        if(!note) {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });
-        }
-        res.send(note);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });                
-        }
-        return res.status(500).send({
-            message: "Error updating note with id " + req.params.noteId
-        });
-    });
-};
+    patient
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: patient._id,
+                message: 'Patient created!',
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Patient not created!',
+            })
+        })
+}
 
-// Delete a note with the specified noteId in the request
-exports.delete = (req, res) => {
-    Note.findByIdAndRemove(req.params.noteId)
-    .then(note => {
-        if(!note) {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });
+updatePatient = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Patient.findOne({ _id: req.params.id }, (err, patient) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Patient not found!',
+            })
         }
-        res.send({message: "Note deleted successfully!"});
-    }).catch(err => {
-        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });                
+        patient.name = body.name
+        patient.time = body.time
+        patient.rating = body.rating
+        patient
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: patient._id,
+                    message: 'Patient updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Patient not updated!',
+                })
+            })
+    })
+}
+
+deletePatient = async (req, res) => {
+    await Patient.findOneAndDelete({ _id: req.params.id }, (err, patient) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
         }
-        return res.status(500).send({
-            message: "Could not delete note with id " + req.params.noteId
-        });
-    });
-};
+
+        if (!patient) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Patient not found` })
+        }
+
+        return res.status(200).json({ success: true, data: patient })
+    }).catch(err => console.log(err))
+}
+
+getPatientById = async (req, res) => {
+    await Patient.findOne({ _id: req.params.id }, (err, patient) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!patient) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Patient not found` })
+        }
+        return res.status(200).json({ success: true, data: patient })
+    }).catch(err => console.log(err))
+}
+
+getPatients = async (req, res) => {
+    await Patient.find({}, (err, patients) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!patients.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Patient not found` })
+        }
+        return res.status(200).json({ success: true, data: patients })
+    }).catch(err => console.log(err))
+}
+
+module.exports = {
+    createPatient,
+    updatePatient,
+    deletePatient,
+    getPatients,
+    getPatientById,
+}
