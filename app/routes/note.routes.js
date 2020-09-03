6d504d8216ec6ea18de5f678dfa3ceb158f190eb
knytcw01 <35956832+knytcw01@users.noreply.github.com>
2020-09-03 10:06:28 +0800

    Update note.routes.js

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,9 +1,13 @@
-module.exports = (app) => {
-    const PatientCtrl = require('../controllers/note.controller.js');
+const express = require('express')
 
-    app.post('/patient', PatientCtrl.createPatient)
-	app.put('/patient/:id', PatientCtrl.updatePatient)
-	app.delete('/patient/:id', PatientCtrl.deletePatient)
-	app.get('/patient/:id', PatientCtrl.getPatientById)
-	app.get('/patients', PatientCtrl.getPatients)
-}
+const PatientCtrl = require('../controllers/note.controller.js')
+
+const router = express.Router()
+
+router.post('/patient', PatientCtrl.createPatient)
+router.put('/patient/:id', PatientCtrl.updatePatient)
+router.delete('/patient/:id', PatientCtrl.deletePatient)
+router.get('/patient/:id', PatientCtrl.getPatientById)
+router.get('/patients', PatientCtrl.getPatients)
+
+module.exports = router
