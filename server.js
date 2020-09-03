e20a141d8e1a3bb9602282ac5b1737c57b2cfec3
knytcw01 <35956832+knytcw01@users.noreply.github.com>
2020-09-03 09:30:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 // create express app
 const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
 
 // parse application/json
 app.use(bodyParser.json())
